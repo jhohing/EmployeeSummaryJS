861602f99477f5b0988933220c45b22a701b8cde
Justin Hohing <jnh0627@yahoo.com>
2021-02-24 22:12:30 -0500

    Started changes to the inquirer prompts and functionality of the app.js file

--- app.js
+++ app.js
@@ -1,191 +1,88 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-var teamList = [];
+let teamList = [];
 const managerQuestions = [
     {
         type: "input",
-        name: "name",
-        message: "Enter manager name:",
-        validate: async (input) => {
-            if (input == "" || /\s/.test(input)) {
-                return "Please enter first or last name.";
-            }
-            return true;
-        }
+        name: "manager_name",
+        message: "Enter manager name:"
     },
     {
         type: "input",
-        name: "email",
-        message: "Enter manager's email:",
-        validate: async (input) => {
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
-                return true;
-            }
-            return "Please enter a valid email address.";
-        }
+        name: "manager_email",
+        message: "Enter manager's email:"
     },
     {
         type: "input",
-        name: "officeNum",
+        name: "manager_officeNum",
         message: "Enter office number:",
-        validate: async (input) => {
-            if (isNaN(input)) {
-                return "Please enter a number";
-            }
-            return true;
-        }
+    }
+];
+const engineerQuestions = [
+    {
+        type:"input",
+        name: "engineer_name",
+        message: "What is your engineer's name?"
     },
+    {
+        type:"input",
+        name: "engineer_id",
+        message: "What is your engineer's id?"
+    },
+    {
+        type:"input",
+        name: "engineer_email",
+        message: "What is your engineer's email?"
+    },
+    {
+        type:"input",
+        name: "engineer_gitHub",
+        message: "What is your engineer's GitHub username?"
+    },
+];
+const internQuestions = [
+    {
+        type:"input",
+        name: "intern_name",
+        message: "What is your intern's name?"
+    },
+    {
+        type:"input",
+        name: "intern_id",
+        message: "What is your intern's id?"
+    },
+    {
+        type:"input",
+        name: "intern_email",
+        message: "What is your intern's email?"
+    },
+    {
+        type:"input",
+        name: "intern_school",
+        message: "What is your intern's school?"
+    },
+];
+const newTeamMember = [
     {
         type: "list",
-        name: "hasTeam",
-        message: "Do you have any team members?",
-        choices: ["Yes", "No"]
+        name:"teamMember_type",
+        choices: ["Engineer", "Intern", "No more team members to add"],
+        message: "Select the role of the team member you want to add?"
     }
-]
+];
 
-const employeeQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "Enter employee name:",
-        validate: async (input) => {
-            if (input == "") {
-                return "Please enter a name.";
-            }
-            return true;
-        }
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter their email:",
-        validate: async (input) => {
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
-                return true;
-            }
-            return "Please enter a valid email address.";
-        }
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "What is their role?",
-        choices: ["engineer", "intern"]
-    },
-    {
-        when: input => {
-            return input.role == "engineer"
-        },
-        type: "input",
-        name: "github",
-        message: "Engineer, enter your github username:",
-        validate: async (input) => {
-            if (input == "" || /\s/.test(input)) {
-                return "Please enter a valid GitHub username";
-            }
-            return true;
-        }
-    },
-    {
-        when: input => {
-            return input.role == "intern"
-        },
-        type: "input",
-        name: "school",
-        message: "Intern, enter your school name:",
-        validate: async (input) => {
-            if (input == "") {
-                return "Please enter a name.";
-            }
-            return true;
-        }
-    },
-    {
-        type: "list",
-        name: "addAnother",
-        message: "Add another team member?",
-        choices: ["Yes", "No"]
-    }
-]
-
-function buildTeamList() {
-    inquire.prompt(employeeQuestions).then(employeeInfo => {
-        if (employeeInfo.role == "engineer") {
-            var newMember = new Engineer(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.github);
-        } else {
-            var newMember = new Intern(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.school);
-        }
-        teamList.push(newMember);
-        if (employeeInfo.addAnother === "Yes") {
-            console.log(" ");
-            buildTeamList();
-        } else {
-            buildHtmlPage();
-        }
-    })
-}
-
-function buildHtmlPage() {
-    let newFile = fs.readFileSync("./templates/main.html")
-    fs.writeFileSync("./output/teamPage.html", newFile, function (err) {
-        if (err) throw err;
-    })
-
-    console.log("Base page generated!");
-
-    for (member of teamList) {
-        if (member.getRole() == "Manager") {
-            buildHtmlCard("manager", member.getName(), member.getId(), member.getEmail(), "Office: " + member.getOfficeNumber());
-        } else if (member.getRole() == "Engineer") {
-            buildHtmlCard("engineer", member.getName(), member.getId(), member.getEmail(), "Github: " + member.getGithub());
-        } else if (member.getRole() == "Intern") {
-            buildHtmlCard("intern", member.getName(), member.getId(), member.getEmail(), "School: " + member.getSchool());
-        }
-    }
-    fs.appendFileSync("./output/teamPage.html", "</div></main></body></html>", function (err) {
-        if (err) throw err;
-    });
-    console.log("Page tags closed! Operation completed.")
-
-}
-
-function buildHtmlCard(memberType, name, id, email, propertyValue) {
-    let data = fs.readFileSync(`./templates/${memberType}.html`, 'utf8')
-    data = data.replace("nameHere", name);
-    data = data.replace("idHere", `ID: ${id}`);
-    data = data.replace("emailHere", `Email: <a href="mailto:${email}">${email}</a>`);
-    data = data.replace("propertyHere", propertyValue);
-    fs.appendFileSync("./output/teamPage.html", data, err => { if (err) throw err; })
-    console.log("Card appended");
-}
-
-function init() {
-    inquire.prompt(managerQuestions).then(managerInfo => {
-        let teamManager = new Manager(managerInfo.name, 1, managerInfo.email, managerInfo.officeNum);
-        teamList.push(teamManager);
-        console.log(" ");
-        if (managerInfo.hasTeam === "Yes") {
-            buildTeamList();    
-        } else {
-            buildHtmlPage();
-        }
-    })
-}
-
-init();
+inquirer.prompt(managerQuestions).then(ans => {
+    console.log(ans);
+    teamList.push(new Manager(ans.man))
+})
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -196,10 +93,6 @@ init();
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
