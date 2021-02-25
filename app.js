891e108002c29cc2fd49c6b88cf272db4bba86bd
Justin Hohing <jnh0627@yahoo.com>
2021-02-24 22:43:12 -0500

    Saved most recent changes on app.js file for engineer and intern prompts and function that creates html file

--- app.js
+++ app.js
@@ -33,44 +33,44 @@ const managerQuestions = [
 ];
 const engineerQuestions = [
     {
-        type:"input",
+        type: "input",
         name: "engineer_name",
         message: "What is your engineer's name?"
     },
     {
-        type:"input",
+        type: "input",
         name: "engineer_id",
         message: "What is your engineer's id?"
     },
     {
-        type:"input",
+        type: "input",
         name: "engineer_email",
         message: "What is your engineer's email?"
     },
     {
-        type:"input",
+        type: "input",
         name: "engineer_gitHub",
         message: "What is your engineer's GitHub username?"
     },
 ];
 const internQuestions = [
     {
-        type:"input",
+        type: "input",
         name: "intern_name",
         message: "What is your intern's name?"
     },
     {
-        type:"input",
+        type: "input",
         name: "intern_id",
         message: "What is your intern's id?"
     },
     {
-        type:"input",
+        type: "input",
         name: "intern_email",
         message: "What is your intern's email?"
     },
     {
-        type:"input",
+        type: "input",
         name: "intern_school",
         message: "What is your intern's school?"
     },
@@ -78,16 +78,63 @@ const internQuestions = [
 const newTeamMember = [
     {
         type: "list",
-        name:"teamMember_type",
+        name: "teamMember_type",
         choices: ["Engineer", "Intern", "No more team members to add"],
         message: "Select the role of the team member you want to add?"
     }
 ];
 
+//creates manager using prompt answers and pushes the answers to the teamList array
 inquirer.prompt(managerQuestions).then(ans => {
     console.log(ans);
-    teamList.push(new Manager(ans.manager_name, ans.manager_id))
-})
+    teamList.push(new Manager(ans.manager_name, ans.manager_id, ans.manager_email, ans.manager_officeNum));
+    nextPrompt();
+});
+
+//prompts if another team member wantsto be added and determines which function to call based on the response
+const nextPrompt = () => {
+    inquirer.prompt(newTeamMember).then(data => {
+        switch (data.teamMember_type) {
+            case "Engineer":
+                promptEngineer();
+                break;
+            case "Intern":
+                promptIntern();
+                break;
+            default:
+                createHTML();
+        }
+    });
+};
+
+//creates engineer using prompt answers and pushes the answers to the teamList array
+const promptEngineer = () => {
+    inquirer.prompt(engineerQuestions).then(ans => {
+        console.log(ans);
+        teamList.push(new Engineer(ans.engineer_name, ans.engineer_id, ans.engineer_email, ans.engineer_gitHub));
+        nextPrompt();
+    });
+};
+
+//creates intern using prompt answers and pushes the answers to the teamList array
+const promptIntern = () => {
+    inquirer.prompt(internQuestions).then(ans => {
+        console.log(ans);
+        teamList.push(new Intern(ans.intern_name, ans.intern_id, ans.intern_email, ans.intern_school));
+        nextPrompt();
+    });
+};
+
+//creates the html to display all the team members
+const createHTML = () => {
+    console.log('this is a new html file');
+    console.log(render(teamList));
+    render(teamList);
+    fs.writeFile("index.html", render(teamList), function(err){
+        if(err) throw err;
+    })
+};
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
